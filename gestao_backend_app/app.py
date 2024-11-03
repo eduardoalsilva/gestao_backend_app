efda39e5505559aa16704e60530050365a02db93
esilva <eduardoalsilva45@gmail.com>
2024-11-03 15:02:20 -0300

    Adicionado schema de mensagem

--- gestao_backend_app/app.py
+++ gestao_backend_app/app.py
@@ -1,7 +1,19 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from gestao_backend_app.schemas import Message
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return """
+    <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+    """
